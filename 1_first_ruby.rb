7fb5069df74adbfb43f172241b5ba1657d8832aa
Andrew Zdunek <andrewzdunek.dev@gmail.com>
2022-06-26 14:58:47 +0100

    adding second draft of ruby exercises

---
+++ 1_first_ruby.rb
@@ -0,0 +1,12 @@
+# puts 
+
+puts "hello world"
+puts "I like Scotties and Westies"
+puts "playing guitar is fun"
+
+# <-- this is a comment hash aka an octothorpe, you can also comment out code
+
+# puts "comment me out"
+
+
+
---
+++ 3_variables_and_names.rb
@@ -0,0 +1,10 @@
+my_name = 'Andrew'
+my_age = 37
+my_height = "?"
+my_weight = 190
+my_eyes = "blue"
+my_hair = "I used to be blonde, now I'm bald"
+
+puts "My name is #{my_name} and I am #{my_age} years old. I am not sure how tall I am #{my_height} 
+but I know I weigh #{my_weight}lbs. and my eyes are #{my_eyes} and for my hair, #{my_hair}."
+
