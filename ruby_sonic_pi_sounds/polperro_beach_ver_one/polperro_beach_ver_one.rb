d5e1d603c64d7a7d8845acd4de69f602c4240757
Andrew Zdunek <andrewzdunek.dev@gmail.com>
2022-06-26 21:08:11 +0100

    adding ruby_sonic_pi_sounds

---
+++ ruby_sonic_pi_sounds/polperro_beach_ver_one/polperro_beach_ver_one.rb
@@ -0,0 +1,11 @@
+
+
+
+loop do
+  
+  sample :ambi_sauna, pan: rrand(-1,1)
+  sleep 5.0
+  
+end
+
+
---
+++ ruby_sonic_pi_sounds/polperro_beach_ver_two/polperro_beach_ver2.rb
@@ -0,0 +1,11 @@
+
+
+
+loop do
+  
+  sample :ambi_sauna, pan: rrand(-1,1)
+  sleep rrand(3, 8)
+  
+end
+
+
